8b17caa78ef817bb42b0c6f2ecd7e17db947da70
Boutabba23 <boutabba.larab@gmail.com>
2025-05-09 23:06:28 +0100

    Comment on Navbar

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 // src/components/layout/Navbar.tsx
-// The new NavBar With CreateNote Button
+// The new NavBar With Create Note Button
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../../store/authStore';
 import { useUIStore } from '../../store/uiStore'; // Import UI store
